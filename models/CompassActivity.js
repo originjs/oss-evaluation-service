b3e5939c94d8d4297ab062dac19116a0920504ef
muclover <guitang001@gmail.com>
2024-02-07 17:35:55 +0800

    feat: add underscored. switch attributes position, and rename attributes

--- models/CompassActivity.js
+++ models/CompassActivity.js
@@ -1,83 +1,76 @@
-import { DataTypes } from 'sequelize';
+import {DataTypes} from 'sequelize';
 import sequelize from '../util/database.js';
 
-export default sequelize.define(
-  'compassActivity',
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      field: 'id',
-    },
-    projectId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: 'project_id',
-    },
-    label: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      field: 'label',
-    },
-    closedIssuesCount: {
-      type: DataTypes.INTEGER,
-      field: 'closed_issues_count',
-    },
-    codeReviewCount: {
-      type: DataTypes.INTEGER,
-      field: 'code_review_count',
-    },
-    commentFrequency: {
-      type: DataTypes.DOUBLE,
-      field: 'comment_frequency',
+export const CompassActivity = sequelize.define(
+    'compassActivity',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            field: 'id'
+        },
+        projectId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            field: 'project_id'
+        },
+        repoUrl: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            field: 'repo_url'
+        },
+        activityScore: {
+            type: DataTypes.DOUBLE,
+            field: 'activity_score'
+        },
+        closedIssuesCount: {
+            type: DataTypes.INTEGER,
+            field: 'closed_issues_count'
+        },
+        codeReviewCount: {
+            type: DataTypes.INTEGER,
+            field: 'code_review_count'
+        },
+        issueCommentFrequency: {
+            type: DataTypes.DOUBLE,
+            field: 'issue_comment_frequency'
 
+        },
+        commitFrequency: {
+            type: DataTypes.DOUBLE,
+            field: 'commit_frequency'
+        },
+        contributorCount: {
+            type: DataTypes.INTEGER,
+            field: 'contributor_count'
+        },
+        orgCount: {
+            type: DataTypes.INTEGER,
+            field: 'org_count'
+        },
+        recentReleasesCount: {
+            type: DataTypes.INTEGER,
+            field: 'recent_releases_count'
+        },
+        updatedIssuesCount: {
+            type: DataTypes.INTEGER,
+            field: 'updated_issues_count'
+        },
+        grimoireCreationDate: {
+            type: DataTypes.TIME,
+            field: 'grimoire_creation_date'
+        },
+        createdAt: {
+            type: DataTypes.TIME,
+            field: 'created_at'
+        }
     },
-    commitFrequency: {
-      type: DataTypes.DOUBLE,
-      field: 'commit_frequency',
-    },
-    contributorCount: {
-      type: DataTypes.INTEGER,
-      field: 'contributor_count',
-
-    },
-    grimoireCreationDate: {
-      type: DataTypes.TIME,
-      field: 'grimoire_creation_date',
-    },
-    orgCount: {
-      type: DataTypes.INTEGER,
-      field: 'org_count',
-    },
-    recentReleasesCount: {
-      type: DataTypes.INTEGER,
-      field: 'recent_releases_count',
-    },
-    updatedIssuesCount: {
-      type: DataTypes.INTEGER,
-      field: 'updated_issues_count',
-    },
-    description: {
-      type: DataTypes.STRING,
-      field: 'description',
-    },
-    isValid: {
-      type: DataTypes.INTEGER,
-      field: 'is_valid',
-    },
-    lastUpdateTime: {
-      type: DataTypes.TIME,
-      field: 'last_update_time',
-    },
-    lastUpdateBy: {
-      type: DataTypes.STRING,
-      field: 'last_update_by',
-    },
-  },
-  {
-    tableName: 'compass_activity',
-    timestamps: false,
-  },
-);
+    {
+        tableName: 'compass_activity_detail',
+        underscored: true,
+        createdAt: true,
+        updatedAt: false
+    }
+)
