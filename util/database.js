626669ee3574e4b70f00af7750ef46784d5dd48c
ruleeeer <70385062+ruleeeer@users.noreply.github.com>
2024-02-08 15:21:31 +0800

    feat: add get chunk size

--- util/database.js
+++ util/database.js
@@ -5,9 +5,6 @@ const sequelize = new Sequelize(
   process.env.DATABASE_URL,
   {
     dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true
-      }
     },
     pool: {
       max: 10,
