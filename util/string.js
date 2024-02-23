3257fd839fbb45448c2034fdc46d34bff94c0055
KonpekiCode <512054675@qq.com>
2024-02-23 16:55:57 +0800

    refactor: generate state_of_js data using loop

--- util/string.js
+++ util/string.js
@@ -1,8 +1,8 @@
 /**
- * format string from underscore to camel case
+ * format string from underscore to small camel case
  * @param {*} str underscore string
- * @returns camel case string
+ * @returns small camel case string
  */
-export function underscoreToCamelCase(str) {
-  return str.replace(/_([a-z])/g, (match, p) => p.toUpperCase());
+export function underscoreToSmallCamelCase(str) {
+  return str.replace(/^[A-Z]/, match => match[0].toLowerCase()).replace(/_([a-z])/g, match => match[1].toUpperCase());
 }
