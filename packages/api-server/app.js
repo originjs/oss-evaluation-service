649c79d9471c8d27a71bb0ce1f707725de107fc4
codeyuefeng <358975191@qq.com>
2024-03-07 11:48:49 +0800

    feat(softwareEcology): add  softwareEcology api
    add  softwareEcology api

--- packages/api-server/app.js
+++ packages/api-server/app.js
@@ -1,8 +1,11 @@
 import express, { json, urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 import indexRouter from './routes/index.js';
+import ecologyRouter from './routes/softwareEcology.js';
 import detailRouter from './routes/softwareDetail.js';
 
 const app = express();
@@ -14,5 +17,25 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/softwareDetail', detailRouter);
+app.use('/', ecologyRouter);
+
+// swagger
+const options = {
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'oss-evaluation-service api-server',
+      version: '0.1.0',
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true }),
+);
 
 export default app;
