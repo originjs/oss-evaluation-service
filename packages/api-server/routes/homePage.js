dda002d3608315f75074f27fbe15d6c732b95b93
Jiannan Zhang <70385062+ruleeeer@users.noreply.github.com>
2024-03-09 10:44:36 +0800

    docs: add swagger doc for api (#60)
    * docs: add swagger doc for api
    
    * fix: field names are underlined

--- packages/api-server/routes/homePage.js
+++ packages/api-server/routes/homePage.js
@@ -4,6 +4,20 @@ import { ok } from '../model/result.js';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /home/search/{keyword}:
+ *   get:
+ *     summary: search
+ *     parameters:
+ *       - in: path
+ *         name: keyword
+ *         required: true
+ *         example: "vue"
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ */
 router.get('/search/:keyword', async (req, res) => {
   const { keyword } = req.params;
   const data = await search(keyword);
