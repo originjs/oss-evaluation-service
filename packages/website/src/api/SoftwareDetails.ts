b16b83b1131181f14488901f789940f49f3346d7
ZekeyWu <wuzejimaya@gmail.com>
2024-03-13 09:58:55 +0800

    feat: Software details page requests interface data

--- packages/website/src/api/SoftwareDetails.ts
+++ packages/website/src/api/SoftwareDetails.ts
@@ -1,11 +1,66 @@
-import HttpRequest from '@api/HttpRequest'
+import HttpRequest from '@api/HttpRequest';
 
-type Performance = {
-  size: number
-  gzipSize: number
-  benchmarkScore: number
+type BaseInfo = {
+  logo: string;
+  description: string;
+  tags: string;
+  star: number;
+  language: string;
+  codeLines: number;
+  firstCommit: string;
+  license: string;
+  evaluation: {
+    functionScore: number;
+    qualityScore: number;
+    performanceScore: number;
+    ecologyScore: number;
+    innovationValue: number;
+  };
+};
+
+export function getBaseInfo(repoName: string) {
+  return HttpRequest.get<BaseInfo>(`/api/softwareDetail/overview/${repoName}`);
 }
 
-export function getPerformance(repoName: string) {
-  return HttpRequest.get<Performance>(`/api/softwareDetail/performance/${repoName}`)
+type functionModuleInfo = {
+  satisfaction: {
+    xAxis: Array<number>;
+    yAxis: Array<number>;
+  };
+  document: {
+    documentScore: number;
+    hasReadme: boolean;
+    hasChangelog: boolean;
+    hasWebsite: boolean;
+    hasContributing: boolean;
+  };
+};
+
+export function getFunctionModuleInfo(repoName: string) {
+  return HttpRequest.get<functionModuleInfo>(`/api/softwareDetail/function/${repoName}`);
+}
+
+type PerformanceModuleInfo = {
+  size: number;
+  gzipSize: number;
+  benchmarkScore: number;
+};
+
+export function getPerformanceModuleInfo(repoName: string) {
+  return HttpRequest.get<PerformanceModuleInfo>(`/api/softwareDetail/performance/${repoName}`);
+}
+
+type QualityModuleInfo = {
+  scorecard: {
+    score: number;
+    maintained: number;
+    codeReview: number;
+    ciiBestPractices: number;
+    license: number;
+    branchProtection: number;
+  };
+};
+
+export function getQualityModuleInfo(repoName: string) {
+  return HttpRequest.get<QualityModuleInfo>(`/api/softwareDetail/quality/${repoName}`);
 }
