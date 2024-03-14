2d3eb65312ea3d73de2b6275c274c7b2f3a51db9
codeyuefeng <47146159+codeyuefeng@users.noreply.github.com>
2024-03-14 11:15:05 +0800

    feat(softwareEcology): add export data to excel (#78)
    add export data to excel

--- packages/api-server/routes/softwareEcology.js
+++ packages/api-server/routes/softwareEcology.js
@@ -1,6 +1,11 @@
 import express from 'express';
-import { ok } from '../model/result.js';
-import { getSoftwareActivity, getSoftwareEcologyOverview } from '../service/softwareEcology.js';
+import debug from 'debug';
+import { fail, ok } from '../model/result.js';
+import {
+  exportExcel,
+  getSoftwareActivity,
+  getSoftwareEcologyOverview,
+} from '../service/softwareEcology.js';
 
 const router = express.Router();
 
@@ -42,6 +47,31 @@ router.get('/ecology/activity/:packageName', async (req, res) => {
   const { packageName } = req.params;
   const softwareActivity = await getSoftwareActivity(packageName);
   res.json(ok(softwareActivity));
+});
+
+/**
+ * @swagger
+ * /ecology/export/{packageName}:
+ *   get:
+ *     summary: export
+ *     parameters:
+ *       - in: path
+ *         name: packageName
+ *         required: true
+ *         example: "vuejs/vue"
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ */
+router.get('/ecology/export/:packageName', async (req, res) => {
+  const { packageName } = req.params;
+  const path = await exportExcel(packageName);
+  if (path === '') {
+    res.json(fail(500, 'export error!'));
+  }
+  res.download(path, 'evaluation.xlsx', (err) => {
+    debug.log(err);
+  });
 });
 
 export default router;
