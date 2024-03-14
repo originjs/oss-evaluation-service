5a0398be6c175d7bb5db3119763abad3db8ad6ec
Chieffo2021 <chieffochen2021@163.com>
2024-03-14 21:17:51 +0800

    feat: search software on input

--- packages/website/src/api/HttpRequest.ts
+++ packages/website/src/api/HttpRequest.ts
@@ -1,10 +1,11 @@
-import axios, {
+import type {
   AxiosInstance,
   AxiosRequestConfig,
   AxiosError,
   AxiosResponse,
   InternalAxiosRequestConfig,
 } from 'axios';
+import axios from 'axios';
 import { ElMessage } from 'element-plus';
 
 enum Code {
@@ -70,8 +71,8 @@ const defaultConfig = {
   withCredentials: false,
   headers: {
     'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+  },
 };
 
 export default new HttpRequest(defaultConfig);
