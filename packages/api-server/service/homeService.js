306b685d76922cceaa9015f0b8a80ad415ba0707
ruleeeer <70385062+ruleeeer@users.noreply.github.com>
2024-03-18 17:27:35 +0800

    feat: only repositories with npm packages will be searched

--- packages/api-server/service/homeService.js
+++ packages/api-server/service/homeService.js
@@ -1,15 +1,20 @@
-import { GithubProjects } from '@orginjs/oss-evaluation-data-model';
-import { Op } from 'sequelize';
+import { sequelize } from '@orginjs/oss-evaluation-data-model';
+import { QueryTypes } from 'sequelize';
 
 export async function search(keyword) {
-  return GithubProjects.findAll({
-    attributes: ['fullName', 'htmlUrl', 'description', 'stargazersCount'],
-    where: {
-      fullName: {
-        [Op.like]: `%${keyword}%`,
-      },
-    },
-    order: [['stargazersCount', 'desc']],
-    limit: 10,
+  const searchSql = `
+    select distinct projects.full_name as fullName,
+           projects.html_url as htmlName,
+           projects.description ,
+           projects.stargazers_count as stargazersCount
+        from github_projects projects
+    join project_packages packages
+    on projects.id = packages.project_id
+    where projects.full_name like concat('%',:keyword,'%') 
+    order by stargazers_count desc
+    limit 10`;
+  return sequelize.query(searchSql, {
+    type: QueryTypes.SELECT,
+    replacements: { keyword },
   });
 }
