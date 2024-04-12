1baf3e435f363cef28e50ca388190b8e37aa852f
codejeffrey <358975191@qq.com>
2024-04-12 09:44:33 +0800

    feat(softwareRank): add syncStargazersTrend function
    add syncStargazersTrend function

---
+++ packages/data-model/models/GithubProjectsStargazersTrend.js
@@ -0,0 +1,63 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../util/database.js';
+
+export default sequelize.define(
+  'GithubProjectsStargazersTrend',
+  {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    projectId: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(512),
+      allowNull: false,
+    },
+    fullName: {
+      type: DataTypes.STRING(512),
+      allowNull: false,
+    },
+    htmlUrl: {
+      type: DataTypes.STRING(512),
+      allowNull: false,
+    },
+    stargazers: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: sequelize.Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+  },
+  {
+    sequelize,
+    tableName: 'github_projects_stargazers_trend',
+    timestamps: false,
+    underscored: true,
+    indexes: [
+      {
+        name: 'PRIMARY',
+        unique: true,
+        using: 'BTREE',
+        fields: [{ name: 'id' }],
+      },
+      {
+        name: 'stargazers_index',
+        unique: true,
+        using: 'BTREE',
+        fields: [{ name: 'full_name' }, { name: 'date' }],
+      },
+    ],
+  },
+);
