715512c787b9758ce7fe3e2b998c3c600bb996fd
codejeffrey <358975191@qq.com>
2024-04-18 11:38:35 +0800

    fix(softwareRank): fix the problem of new data added every month
    fix the problem of new data added every month

--- packages/api-server/src/services/TrendService.ts
+++ packages/api-server/src/services/TrendService.ts
@@ -87,10 +87,9 @@ export async function githubTop(page: Page, type: string) {
 
     const monthDiff = softwareTrend
       .map((current, index, array) => {
-        if (index === 0) return undefined;
+        if (index === 0) return 0;
         return current.stargazers - array[index - 1].stargazers;
       })
-      .slice(1);
 
     const monthCount = _.pluck(softwareTrend, 'stargazers');
     data.push({
