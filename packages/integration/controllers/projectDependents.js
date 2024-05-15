26b13924a7755e40ff02073c68058a3312423955
WilliamHeaven <2403306615@qq.com>
2024-05-15 09:39:17 +0800

    Fix the issue with the number of dependencies

--- packages/integration/controllers/projectDependents.js
+++ packages/integration/controllers/projectDependents.js
@@ -1,13 +1,27 @@
 import debug from 'debug';
-import { GithubProjects } from '@orginjs/oss-evaluation-data-model';
+import { GithubProjects, ProjectPackage} from '@orginjs/oss-evaluation-data-model';
 import { CheerioCrawler, Configuration } from 'crawlee';
 import { Cron } from 'croner';
+GithubProjects.hasMany(ProjectPackage, {
+  foreignKey: 'project_id',
+  as: 'projectPackage',
+});
 
 export default async function syncProjectDependentCount(req, res) {
   debug.log('Sync Project dependent count');
   // 1. get all github project
   const { projectId: projectId } = req.params;
   const projectList = await GithubProjects.findAll({
+    include: [
+      {
+        model: ProjectPackage,
+        as: 'projectPackage',
+        required: false,
+        where: {
+          main_package: 1
+        }
+      },
+    ],
     attributes: ['id', 'htmlUrl', 'dependentRepositories', 'dependentPackages'],
     where: projectId
       ? {
@@ -22,7 +36,9 @@ export default async function syncProjectDependentCount(req, res) {
     debug.log('**Current Progress**: ', `${count}/${sumOfProject}`);
     count += 1;
     // 2. get project dependent count
-    const dependentCount = await getProjectDependentCount(`${project.htmlUrl}/network/dependents`);
+    let url = `${project.htmlUrl}/network/dependents`;
+    url = await getProjectMainPackageUrl(url, project.projectPackage.length > 0 ? project.projectPackage[0].package : "");
+    const dependentCount = await getProjectDependentCount(url);
     if (
       dependentCount.repositories != undefined &&
       dependentCount.packages != undefined &&
@@ -82,6 +98,49 @@ async function getProjectDependentCount(url) {
     debug.log(`**Url get dependent count is failed !** :${url}`);
   }
   return { repositories: repos, packages: packs };
+}
+
+async function getProjectMainPackageUrl(url, packageName) {
+  if (packageName === "" || packageName === undefined) {
+    return url;
+  }
+  let urlResult = url;
+  const config = new Configuration({ persistStorage: false });
+  const crawler = new CheerioCrawler(
+    {
+      failedRequestHandler({ request, log }) {
+        log.info(`Get project main package url: Request to ${request.url} failed...`);
+      },
+      requestHandler: async function({$, log }) {
+        const context = $(`.select-menu-item span:contains(" ${packageName}\n")`);
+        const selectContext = context.text().replaceAll('\n', '').replaceAll(' ', '');
+        if (context.length > 0 && selectContext === packageName) {
+          const attrs = context.get(0).parent.attributes;
+          attrs.forEach(attr => {
+            if (attr.name === "href") {
+              const href = attr.value;
+              const index = href.indexOf("?");
+              const urlSuffix = href.substring(index);
+              urlResult = urlResult.concat(urlSuffix);
+            }
+          })
+        }
+        log.info(`Get project main package url: ${urlResult} `);
+      },
+      requestHandlerTimeoutSecs: 60,
+      maxRequestsPerCrawl: 10,
+      maxRequestRetries: 1,
+      maxConcurrency: 4029,
+      sameDomainDelaySecs: 10,
+    },
+    config,
+  );
+  try {
+    await crawler.run([url]);
+  } catch (e) {
+    debug.log(`**Get project main package url is failed !** :${url}`);
+  }
+  return urlResult;
 }
 
 const errorHandler = e => {
