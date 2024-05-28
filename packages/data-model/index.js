ba55d31210ef2e6121250969ce97249f07db79cf
wangsongc <wangsongc@foxmail.com>
2024-05-28 22:18:28 +0800

    fix: api-server start error

--- packages/data-model/index.js
+++ packages/data-model/index.js
@@ -28,6 +28,6 @@ export { default as StateOfJsMin } from './models/StateOfJsMin.js';
 export { default as SonarCloudProjectMin } from './models/SonarCloudProjectMin.js';
 export { default as GithubProjectsStargazersTrend } from './models/GithubProjectsStargazersTrend.js';
 export { default as GithubProjectsDependencies } from './models/GithubProjectsDependencies.js';
-// export { default as OssinsightPullRequestCreatorsCountries } from './models/OssinsightPullRequestCreatorsCountries.js';
+export { default as OssinsightPullRequestCreatorsCountries } from './models/OssinsightPullRequestCreatorsCountries.js';
 export { default as OssinsightCreatorsOrganizations } from './models/OssinsightCreatorsOrganizations.js';
 export { default as OssinsightCreatorsCountries } from './models/OssinsightCreatorsCountries.js';
