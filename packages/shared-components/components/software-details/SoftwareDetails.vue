150156c4759a2cfc5c9c190c1396f25d77ca490e
ZekeyWu <wuzejimaya@gmail.com>
2024-05-29 15:01:18 +0800

    improvements: scatter point size

--- packages/shared-components/components/software-details/SoftwareDetails.vue
+++ packages/shared-components/components/software-details/SoftwareDetails.vue
@@ -555,7 +555,7 @@ function renderCountriesChart(selector, data) {
       }
       return {
         name: countriesNameMap[countryInfo.code] ?? '',
-        value: [countryInfo.long, countryInfo.lat, country.creatorsNum],
+        value: [countryInfo.long, countryInfo.lat, country.creatorsNum, country.percentage],
       };
     })
     .filter(country => country);
@@ -596,9 +596,9 @@ function renderCountriesChart(selector, data) {
         name: props.repoName,
         type: 'scatter',
         coordinateSystem: 'geo',
-        data: countriesData.slice(1),
+        data: countriesData.slice(1, 100),
         symbolSize: function (val) {
-          return Math.log2(val[2]) * 3;
+          return Math.max(Math.sqrt(val[3] * 50000 / Math.PI), 4);
         },
       },
       {
@@ -607,7 +607,7 @@ function renderCountriesChart(selector, data) {
         coordinateSystem: 'geo',
         data: countriesData[0] ? [countriesData[0]] : [],
         symbolSize: function (val) {
-          return Math.log2(val[2]) * 3;
+          return Math.max(Math.sqrt(val[3] * 50000 / Math.PI), 4);
         },
         label: {
           formatter: '{b}',
@@ -1594,12 +1594,12 @@ onBeforeUnmount(() => {
         <el-tab-pane label="star" name="star">
           <div flex flex-items-start mb-6>
             <el-card w-964px mr-4>
-              <div mb-2 font-size-5 font-bold>点赞人员地理分布</div>
+              <div mb-2 font-size-5 font-bold>star人员地理分布</div>
               <div id="star-countries-chart" h-500px />
             </el-card>
             <el-card w-300px>
               <div mb-2 font-size-5 font-bold>Top 10</div>
-              <template v-if="project?.starCountries.length">
+              <template v-if="project?.starCountries?.length">
                 <div
                   v-for="(country, idx) in project.starCountries.slice(0, 10)"
                   :key="idx"
@@ -1636,7 +1636,7 @@ onBeforeUnmount(() => {
             </el-card>
             <el-card w-300px>
               <div mb-2 font-size-5 font-bold>Top 10</div>
-              <template v-if="project?.issueCountries.length">
+              <template v-if="project?.issueCountries?.length">
                 <div
                   v-for="(country, idx) in project.issueCountries.slice(0, 10)"
                   :key="idx"
@@ -1673,7 +1673,7 @@ onBeforeUnmount(() => {
             </el-card>
             <el-card w-300px>
               <div mb-2 font-size-5 font-bold>Top 10</div>
-              <template v-if="project?.prCountries.length">
+              <template v-if="project?.prCountries?.length">
                 <div
                   v-for="(country, idx) in project.prCountries.slice(0, 10)"
                   :key="idx"
