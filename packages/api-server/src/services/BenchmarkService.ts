6f492c727b9cf8393e8cf72a6cc83c952924b2e6
ruleeeer <70385062+ruleeeer@users.noreply.github.com>
2024-07-29 21:16:04 +0800

    feat: add proxy for import benchmark

--- packages/api-server/src/services/BenchmarkService.ts
+++ packages/api-server/src/services/BenchmarkService.ts
@@ -10,6 +10,7 @@ import type {
 } from '../interfaces/SoftwareInfo.js';
 import { fixedRound } from '../utils/math.js';
 import { Op } from 'sequelize';
+import { ProxyAgent } from 'undici';
 
 const sleep = ms =>
   new Promise(resolve => {
@@ -275,25 +276,32 @@ async function importBenchmarkData(data: { benchmark: BenchmarkValue[]; index: B
   if (!process.env.INTEGRATION_URL) {
     throw new Error('no env named {INTEGRATION_URL} , skip import');
   }
+  const requestOpts = {
+    method: 'GET',
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Accept-Encoding': 'gzip, deflate, br',
+      Connection: 'keep-alive',
+      'Upgrade-Insecure-Requests': '1',
+      'Cache-Control': 'max-age=0',
+    },
+  };
+  const proxyUrl = process.env.PROXY_URL;
+  if (proxyUrl) {
+    const client = new ProxyAgent(proxyUrl);
+    // @ts-expect-error no need handle
+    requestOpts.dispatcher = client;
+  }
   const requestFn = async (urlParam: string, arr: unknown[]) => {
     for (const obj of arr) {
       const url = new URL(urlParam);
       Object.getOwnPropertyNames(obj).forEach(key => {
         url.searchParams.append(key, obj[key]);
       });
-      const importResponse = await fetch(url.href, {
-        method: 'GET',
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-          Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-          'Accept-Language': 'en-US,en;q=0.5',
-          'Accept-Encoding': 'gzip, deflate, br',
-          Connection: 'keep-alive',
-          'Upgrade-Insecure-Requests': '1',
-          'Cache-Control': 'max-age=0',
-        },
-      });
+      const importResponse = await fetch(url.href, requestOpts);
       if (!importResponse.ok) {
         throw new Error(
           `call api to import benchmark data failed ${url.href}! , ${await importResponse.text()}`,
