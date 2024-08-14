ca70efa69e8700caa5a5db44ffc22a7a98b495c8
TRebirthC <2223055232@qq.com>
2024-08-14 16:11:41 +0800

    fix: update the table name in oss-evaluate-summary-history.sql

--- sql/oss-evaluate-summary-history.sql
+++ sql/oss-evaluate-summary-history.sql
@@ -1,5 +1,5 @@
--- create table 
-CREATE TABLE oss-evaluate-summary-history (
+-- create table oss_evaluate_summary_history to record the history evaluate score
+CREATE TABLE oss_evaluate_summary_history (
     id INT AUTO_INCREMENT PRIMARY KEY,
     project_id int NOT NULL,
     date DATE NOT NULL,
