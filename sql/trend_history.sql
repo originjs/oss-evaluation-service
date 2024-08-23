f2affb15e2e30775f151bb354f90b9a1748d84ec
TRebirthC <2223055232@qq.com>
2024-08-23 15:03:21 +0800

    feat: add a table to store historical trend information

---
+++ sql/trend_history.sql
@@ -0,0 +1,11 @@
+-- create table trend_history
+CREATE TABLE trend_history (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    project_id int NOT NULL,
+    data_type int NOT NULL DEFAULT 0,
+    increased_value DOUBLE NOT NULL DEFAULT -1,
+    total_value DOUBLE NOT NULL DEFAULT -1,
+    date_type int NOT NULL DEFAULT 0,
+    date DATE NOT NULL,
+    UNIQUE(project_id, data_type, date_type, date)
+);
