6595f16cc75e202337d70b444d3da21242a000ba
TRebirthC <2223055232@qq.com>
2024-09-02 16:57:16 +0800

    feat: implement the interface of the new trend list

--- packages/api-server/src/services/TrendService.ts
+++ packages/api-server/src/services/TrendService.ts
@@ -1,9 +1,13 @@
-import { GithubProjects } from '@orginjs/oss-evaluation-data-model';
 import {
+  GithubProjects,
+  TrendHistory,
   EvaluationSummary,
   GithubProjectsStargazersTrend,
+  logger,
 } from '@orginjs/oss-evaluation-data-model';
 import _ from 'underscore';
+import { Op } from 'sequelize';
+import dayjs from 'dayjs';
 
 export class ChartData {
   monthCount: number[];
@@ -42,6 +46,11 @@ const typeMap = new Map();
 typeMap.set('star', 'stargazersCount');
 typeMap.set('fork', 'forksCount');
 typeMap.set('contributors', 'contributors');
+
+const STAGE_TYPE = {
+  CURRENT: 1,
+  LAST: 0,
+};
 
 GithubProjects.hasOne(EvaluationSummary, {
   foreignKey: 'project_id',
@@ -98,6 +107,125 @@ export async function githubTop(page: Page, type: string) {
       trend: new ChartData(monthCount, monthDiff),
     });
   }
+  const res = Page.clone(page);
+  res.data = data;
+  return res;
+}
+
+async function getTrendData(type, stageType) {
+  // stageType为1表明当前阶段数据，stageType为0表示上一阶段数据
+  const dataType = type.dataType;
+  const dateType = type.dateType;
+  const rankType = type.rankType;
+  let orderCriteria;
+  if (rankType === 1) {
+    orderCriteria = [
+      ['date', 'ASC'],
+      ['increasedValue', 'DESC'],
+    ];
+  } else {
+    orderCriteria = [
+      ['date', 'ASC'],
+      ['totalValue', 'DESC'],
+    ];
+  }
+
+  let date = dayjs();
+  let startDate, endDate;
+
+  // 年数据处理
+  if (dateType == 1) {
+    if (stageType === STAGE_TYPE.LAST) {
+      date = date.subtract(1, 'year');
+    }
+    startDate = date.startOf('year').toDate();
+    startDate = date.add(1, 'year').startOf('year').toDate();
+  }
+  // 月数据处理
+  if (dateType == 2) {
+    if (stageType === STAGE_TYPE.LAST) {
+      date = date.subtract(1, 'month');
+    }
+    startDate = date.startOf('month').toDate();
+    endDate = date.add(1, 'month').startOf('month').toDate();
+  }
+  const result = await TrendHistory.findAll({
+    attributes: ['projectId', 'increasedValue', 'totalValue', 'date'],
+    where: {
+      date: {
+        [Op.gte]: startDate,
+        [Op.lt]: endDate,
+      },
+      dataType: dataType,
+      dateType: dateType,
+    },
+    order: orderCriteria,
+  }).catch(err => {
+    logger.error('Error occurred:', err);
+  });
+  // 去重，保留更早的数据
+  const firstDataMap = new Map();
+  const deduplicatedResults = result.filter(data => {
+    const projectId = data.projectId;
+    if (!firstDataMap.has(projectId)) {
+      firstDataMap.set(projectId, true);
+      return true;
+    }
+    return false;
+  });
+  return deduplicatedResults;
+}
+
+export async function newGithubTop(
+  page: Page,
+  type: { dataType: string; dateType: string; rankType: string },
+) {
+  const pageSize = page.pageSize;
+  const offset = page.pageSize * (page.pageNo - 1);
+
+  const currentTrendData = await getTrendData(type, 1);
+  const result = currentTrendData.slice(offset, offset + pageSize);
+
+  // 上一阶段的排名数据
+  const lastTrendData = await getTrendData(type, 0);
+  lastTrendData.forEach((item, index) => {
+    item.lastRank = index + 1;
+  });
+  const rankMap = new Map();
+  lastTrendData.forEach(item => {
+    rankMap.set(item.projectId, item.lastRank);
+  });
+
+  const data = [];
+  let index = offset;
+  for (const item of result) {
+    index++;
+    let lastPeriodRank;
+    if (rankMap.has(item.projectId)) {
+      lastPeriodRank = rankMap.get(item.projectId);
+    } else {
+      lastPeriodRank = null;
+    }
+
+    const projectInfo = await GithubProjects.findOne({
+      where: {
+        id: item.projectId,
+      },
+      attributes: ['fullName', 'htmlUrl', 'description', 'ownerAvatarUrl'],
+    });
+
+    data.push({
+      currentRank: index,
+      lastRank: lastPeriodRank,
+      increasedValue: item.increasedValue,
+      totalValue: item.totalValue,
+      name: projectInfo.fullName,
+      logo: projectInfo.ownerAvatarUrl,
+      htmlUrl: projectInfo.htmlUrl,
+      description: projectInfo.description,
+    });
+  }
+
   const res = Page.clone(page);
   res.data = data;
   return res;
