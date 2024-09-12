ef7ea215afb8517bddb6bbb64fa0e8a0d1d84bde
ruleeeer <70385062+ruleeeer@users.noreply.github.com>
2024-09-12 20:54:11 +0800

    fix: close trend github timer avoid a conflict

--- packages/integration/scheduler/config.js
+++ packages/integration/scheduler/config.js
@@ -38,10 +38,12 @@ export const JobConfig = {
       cronScheduleTime: '0 0 1 * *', // 每月1号 00:00
       enabled: isProduction, // only start in production environment
     },
+    // 下面的定时器用于爬取github的页面(非api接口)的star和contributor供趋势榜单使用，由于需要访问github页面，所以最好在海外或者有代理的机器上执行
+    // 目前在提供sonarCloud扫描服务的机器上执行，关闭其他服务器上的执行防止冲突
     {
       name: 'projectHistoryTimer',
       cronScheduleTime: '0 0 * * *', // 每天 00:00执行，实际会在代码中控制每月1号和每周的第一天执行
-      enabled: isProduction, // only start in production environment
+      enabled: false,
     },
     {
       name: 'githubProjectsDailyTimer',
