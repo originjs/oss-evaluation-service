fa88f2479067169a352b43dd3711c850bfb8c425
ruleeeer <70385062+ruleeeer@users.noreply.github.com>
2024-09-18 10:41:58 +0800

    fix: remove trend service timer

--- packages/integration/scheduler/config.js
+++ packages/integration/scheduler/config.js
@@ -33,13 +33,13 @@ export const JobConfig = {
       cronScheduleTime: '0 0 * * 6', // 周六 00:00
       enabled: isProduction, // only start in production environment
     },
+    // 下面的定时器用于爬取github的页面(非api接口)的star和contributor供趋势榜单使用，由于需要访问github页面，所以最好在海外或者有代理的机器上执行
+    // 目前在提供sonarCloud扫描服务的机器上执行，关闭其他服务器上的执行防止冲突
     {
       name: 'evaluateTimer',
       cronScheduleTime: '0 0 1 * *', // 每月1号 00:00
-      enabled: isProduction, // only start in production environment
+      enabled: false, // only start in production environment
     },
-    // 下面的定时器用于爬取github的页面(非api接口)的star和contributor供趋势榜单使用，由于需要访问github页面，所以最好在海外或者有代理的机器上执行
-    // 目前在提供sonarCloud扫描服务的机器上执行，关闭其他服务器上的执行防止冲突
     {
       name: 'projectHistoryTimer',
       cronScheduleTime: '0 0 * * *', // 每天 00:00执行，实际会在代码中控制每月1号和每周的第一天执行
@@ -48,12 +48,12 @@ export const JobConfig = {
     {
       name: 'githubProjectsDailyTimer',
       cronScheduleTime: '0 0 * * *', // 每天 00:00
-      enabled: isProduction, // only start in production environment
+      enabled: false, // only start in production environment
     },
     {
       name: 'githubProjectsWeeklyTimer',
       cronScheduleTime: '0 0 * * 1', // 周一 00:00
-      enabled: isProduction, // only start in production environment
+      enabled: false, // only start in production environment
     },
   ],
 };
