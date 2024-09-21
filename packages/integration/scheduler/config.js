c1740aec89235b307831add68690b120e2c1a34e
ruleeeer <70385062+ruleeeer@users.noreply.github.com>
2024-09-21 10:11:20 +0800

    feat: enable timer for trend data

--- packages/integration/scheduler/config.js
+++ packages/integration/scheduler/config.js
@@ -6,27 +6,22 @@ export const JobConfig = {
     {
       name: 'evaluateTimer',
       cronScheduleTime: '0 0 1 * *', // 每月1号 00:00
-      enabled: false, // only start in production environment
+      enabled: true, // only start in production environment
     },
     {
       name: 'projectHistoryTimer',
       cronScheduleTime: '0 0 * * *', // 每天 00:00执行，实际会在代码中控制每月1号和每周的第一天执行
-      enabled: false,
+      enabled: true,
     },
     {
       name: 'githubProjectsDailyTimer',
       cronScheduleTime: '0 0 * * *', // 每天 00:00
-      enabled: false, // only start in production environment
+      enabled: true, // only start in production environment
     },
     {
       name: 'githubProjectsWeeklyTimer',
       cronScheduleTime: '0 0 * * 1', // 周一 00:00
       enabled: false, // only start in production environment
-    },
-    {
-      name: 'githubStargazersTrendTimer',
-      cronScheduleTime: '0 12 5 * *', // 每月5号 12:00
-      enabled: isProduction, // only start in production environment
     },
   ],
 };
