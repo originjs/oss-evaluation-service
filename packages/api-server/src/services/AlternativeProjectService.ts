dddacc43df8fb4f8322b451bb1c1b29f2bfdc315
Chieffo2021 <chieffochen2021@163.com>
2025-03-18 17:02:21 +0800

    optimize: mark similar category project as non-AI

--- packages/api-server/src/services/AlternativeProjectService.ts
+++ packages/api-server/src/services/AlternativeProjectService.ts
@@ -6,10 +6,20 @@ AlternativeProjects.hasOne(GithubProjects, { foreignKey: 'alternative_id' });
 export async function getAlternativeProjects(fullName: string): Promise<AlternativeInfo[]> {
   const ALTERNATIVE_SIZE = 6;
   // similar project by ai
-  const sql = `SELECT alternative_id, alternative_name, alternative_url, source, 
-       owner_avatar_url, description, stargazers_count as starCount, forks_count as forksCount
-  FROM alternative_projects a LEFT JOIN github_projects g ON a.alternative_id=g.id
-  WHERE a.full_name=:fullName AND approved=1 ORDER BY distance LIMIT ${ALTERNATIVE_SIZE}`;
+  let sql = `SELECT alternative_id,
+                    alternative_name,
+                    alternative_url,
+                    source,
+                    owner_avatar_url,
+                    description,
+                    stargazers_count as starCount,
+                    forks_count      as forksCount
+             FROM alternative_projects a
+                      LEFT JOIN github_projects g ON a.alternative_id = g.id
+             WHERE a.full_name = :fullName
+               AND approved = 1
+             ORDER BY distance
+             LIMIT ${ALTERNATIVE_SIZE}`;
   let list = await sequelize.query(sql, {
     replacements: { fullName },
     type: sequelize.QueryTypes.SELECT,
@@ -26,22 +36,34 @@ export async function getAlternativeProjects(fullName: string): Promise<Alternat
       ai: item.source === 'ai' ? 1 : 0,
     };
   });
-  if (alternatives.length < ALTERNATIVE_SIZE) {
-    // similar project by subcategory
-    const sql = `SELECT id, g.full_name, g.html_url, g.owner_avatar_url as logo, 
-       description, g.stargazers_count as starCount, g.forks_count as forksCount
-FROM github_projects g JOIN project_tech_stack t ON g.id = t.project_id 
-WHERE subcategory IN ( SELECT subcategory FROM project_tech_stack WHERE html_url = :repoName ) 
-ORDER BY stargazers_count DESC LIMIT ${ALTERNATIVE_SIZE + 1}`;
-    list = await sequelize.query(sql, {
-      replacements: { repoName: `https://github.com/${fullName}` },
-      type: sequelize.QueryTypes.SELECT,
-    });
-    for (const item of list) {
-      // exclude it self
-      if (item.full_name === fullName) continue;
-      // exclude exist
-      if (alternatives.find(e => e.repoName === item.full_name)) continue;
+  // similar project by category and subcategory
+  sql = `SELECT id,
+                g.full_name,
+                g.html_url,
+                g.owner_avatar_url as logo,
+                description,
+                g.stargazers_count as starCount,
+                g.forks_count      as forksCount
+         FROM github_projects g
+                  JOIN project_tech_stack t ON g.id = t.project_id
+         WHERE category IN (SELECT category FROM project_tech_stack WHERE html_url = :repoName)
+           and subcategory IN (SELECT subcategory FROM project_tech_stack WHERE html_url = :repoName)
+         ORDER BY stargazers_count DESC`;
+  list = await sequelize.query(sql, {
+    replacements: { repoName: `https://github.com/${fullName}` },
+    type: sequelize.QueryTypes.SELECT,
+  });
+  for (const item of list) {
+    // exclude it self
+    if (item.full_name === fullName) continue;
+
+    const alternative = alternatives.find(e => e.repoName === item.full_name);
+    if (alternative) {
+      alternative.ai = 0; // mark as non-AI
+      continue; // exclude exist
+    }
+
+    if (alternatives.length < ALTERNATIVE_SIZE) {
       alternatives.push({
         id: item.id,
         repoName: item.full_name,
@@ -51,9 +73,8 @@ ORDER BY stargazers_count DESC LIMIT ${ALTERNATIVE_SIZE + 1}`;
         url: item.html_url,
         description: item.description,
       });
-
-      if (alternatives.length >= ALTERNATIVE_SIZE) break;
     }
   }
+
   return alternatives;
 }
