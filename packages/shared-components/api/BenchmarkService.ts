16ef2be9c7935a333e9b945e32005e04eef36dbc
Chieffo2021 <chieffochen2021@163.com>
2025-03-22 15:35:06 +0800

    feat: support dynamically display benchmark tech stack

--- packages/shared-components/api/BenchmarkService.ts
+++ packages/shared-components/api/BenchmarkService.ts
@@ -1,11 +1,16 @@
 import HttpRequest from './HttpRequest';
+import type { BenchmarkTechStack } from '@orginjs/oss-evaluation-api-server';
 import type {
   BenchmarkIndex,
   SoftwareBaseInfo,
   BenchmarkResult,
 } from '@orginjs/oss-evaluation-api-server';
 
-export { BenchmarkIndex, SoftwareBaseInfo, BenchmarkResult };
+export type { BenchmarkIndex, SoftwareBaseInfo, BenchmarkResult };
+
+export function getTechStacks() {
+  return HttpRequest.get<BenchmarkTechStack[]>(`/benchmark/techStacks`);
+}
 
 export function getProjectsByTechStack(category: string, techStack: string) {
   return HttpRequest.get<Array<SoftwareBaseInfo>>(`/benchmark/techstack/${category}/${techStack}`);
