2491a5edf0674f24b86339e14061fa1b5fea6bce
Jiannan Zhang <70385062+ruleeeer@users.noreply.github.com>
2025-04-21 09:27:31 +0800

    fix: update job timers to enable in production environment

--- packages/integration/scheduler/config.js
+++ packages/integration/scheduler/config.js
@@ -33,27 +33,25 @@ export const JobConfig = {
       cronScheduleTime: '0 0 * * 6', // 周六 00:00
       enabled: isProduction, // only start in production environment
     },
-    // 下面的定时器用于爬取github的页面(非api接口)的star和contributor供趋势榜单使用，由于需要访问github页面，所以最好在海外或者有代理的机器上执行
-    // 目前在提供sonarCloud扫描服务的机器上执行，关闭其他服务器上的执行防止冲突
     {
       name: 'evaluateTimer',
       cronScheduleTime: '0 0 1 * *', // 每月1号 00:00
-      enabled: false, // only start in production environment
+      enabled: isProduction, // only start in production environment
     },
     {
       name: 'projectHistoryTimer',
       cronScheduleTime: '0 0 * * *', // 每天 00:00执行，实际会在代码中控制每月1号和每周的第一天执行
-      enabled: false,
+      enabled: isProduction,
     },
     {
       name: 'githubProjectsDailyTimer',
       cronScheduleTime: '0 0 * * *', // 每天 00:00
-      enabled: false, // only start in production environment
+      enabled: isProduction, // only start in production environment
     },
     {
       name: 'githubProjectsWeeklyTimer',
       cronScheduleTime: '0 0 * * 1', // 周一 00:00
-      enabled: false, // only start in production environment
+      enabled: isProduction, // only start in production environment
     },
     {
       name: 'githubStargazersTrendTimer',
