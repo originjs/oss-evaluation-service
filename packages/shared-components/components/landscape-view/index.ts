3cb206ec4c851fbcc46e80a0c5813015e74d5c1e
Chieffo2021 <chieffochen2021@163.com>
2025-05-09 11:57:27 +0800

    feat: support landscape third view

--- packages/shared-components/components/landscape-view/index.ts
+++ packages/shared-components/components/landscape-view/index.ts
@@ -1,4 +1,5 @@
 export { default as LandscapeView } from './LandscapeView.vue';
+export { default as LandscapeThreeView } from './LandscapeThreeView.vue';
 export { default as ProjectPopover } from './ProjectPopover.vue';
 export { default as GenerateProjectAvatar } from './GenerateProjectAvatar.vue';
 export { default as ProjectThumbnails } from './ProjectThumbnails.vue';
