6a6cc6bcf5a95946e8cd5d63dd839f63f2ca0df1
Chieffo2021 <chieffochen2021@163.com>
2025-05-13 10:38:20 +0800

    fix: retry when fetching chat failed

--- packages/api-sdk/extChat.js
+++ packages/api-sdk/extChat.js
@@ -1,17 +1,32 @@
 import { authorizationHeader } from './util.js';
+import { logger } from '../data-model/index.js';
 
-export function chat(data, token) {
+export async function chat(data, token) {
   const headers = authorizationHeader(token);
   headers.append('Content-Type', 'application/json');
   headers.append('Accept', '*/*');
   headers.append('Connection', 'keep-alive');
-  return fetch(process.env.EXT_AI_SERVICE_URL, {
-    method: 'POST',
-    headers,
-    body: JSON.stringify({
-      inputs: data,
-      response_mode: 'blocking',
-      user: 'abc-123',
-    }),
-  });
+  let response;
+  let retryCount = 0;
+  while (retryCount !== -1 && retryCount < 5) {
+    try {
+      response = await fetch(process.env.EXT_AI_SERVICE_URL, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({
+          inputs: data,
+          response_mode: 'blocking',
+          user: 'abc-123',
+        }),
+      });
+      retryCount = -1;
+    } catch (e) {
+      retryCount++;
+      if (retryCount === 5) {
+        throw e;
+      }
+      logger.error(`Fetch chat failed! Retry count: ${retryCount}\n`, e);
+    }
+  }
+  return response;
 }
