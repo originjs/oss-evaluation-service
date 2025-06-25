336316f4223bd648e6519b8dbcba66ce1d9260b1
Chieffo2021 <chieffochen2021@163.com>
2025-06-25 15:40:06 +0800

    fix: filed error in sql

--- packages/integration/controllers/alternative.js
+++ packages/integration/controllers/alternative.js
@@ -46,7 +46,7 @@ export async function syncAlternativeHandler(req, res) {
 }
 
 export async function syncAllProjectAlternative() {
-  let sql = `SELECT p.p_id, p.full_name, p.html_url, p.id, p.platformType
+  let sql = `SELECT p.p_id, p.full_name, p.html_url, p.id, p.platform_type
              from view_projects p
                       LEFT JOIN project_tech_stack t
                                 ON p.p_id = t.p_id
