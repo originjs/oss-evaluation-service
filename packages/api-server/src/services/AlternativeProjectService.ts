7c537e624a6ec334c42bc833e94916f519a71c9b
Chieffo2021 <chieffochen2021@163.com>
2025-11-03 11:22:36 +0800

    fix: preserve AI mark and query parallelism

--- packages/api-server/src/services/AlternativeProjectService.ts
+++ packages/api-server/src/services/AlternativeProjectService.ts
@@ -5,39 +5,23 @@ AlternativeProjects.hasOne(ViewProjects, { foreignKey: 'p_id' });
 
 export async function getAlternativeProjects(fullName: string): Promise<AlternativeInfo[]> {
   const ALTERNATIVE_SIZE = 6;
-  // similar project by ai
-  let sql = `SELECT alternative_id,
-                    alternative_name,
-                    alternative_url,
-                    source,
-                    owner_avatar_url,
-                    description,
-                    stargazers_count as starCount,
-                    forks_count      as forksCount
-             FROM alternative_projects a
-                      LEFT JOIN view_projects p ON a.alternative_id = p.p_id
-             WHERE a.full_name = :fullName
-               AND approved = 1
-             ORDER BY distance
-             LIMIT ${ALTERNATIVE_SIZE}`;
-  let list = await sequelize.query(sql, {
-    replacements: { fullName },
-    type: sequelize.QueryTypes.SELECT,
-  });
-  const alternatives = list.map(item => {
-    return {
-      pId: item.alternative_id,
-      repoName: item.alternative_name,
-      logo: item.owner_avatar_url,
-      starCount: item.starCount,
-      forksCount: item.forksCount,
-      url: item.alternative_url,
-      description: item.description,
-      ai: item.source === 'ai' ? 1 : 0,
-    };
-  });
+  // similar project by AI
+  const querySimilarByAiSql = `SELECT alternative_id,
+                                      alternative_name,
+                                      alternative_url,
+                                      source,
+                                      owner_avatar_url,
+                                      description,
+                                      stargazers_count as starCount,
+                                      forks_count      as forksCount
+                               FROM alternative_projects a
+                                        LEFT JOIN view_projects p ON a.alternative_id = p.p_id
+                               WHERE a.full_name = :fullName
+                                 AND approved = 1
+                               ORDER BY distance, stargazers_count DESC
+                               LIMIT ${ALTERNATIVE_SIZE}`;
   // similar project by category and subcategory
-  sql = `SELECT p.p_id,
+  const querySimilarByCategorySql = `SELECT p.p_id,
                 p.full_name,
                 p.html_url,
                 p.owner_avatar_url as logo,
@@ -49,17 +33,34 @@ export async function getAlternativeProjects(fullName: string): Promise<Alternat
          WHERE category IN (SELECT category FROM project_tech_stack WHERE html_url = :repoName)
            and subcategory IN (SELECT subcategory FROM project_tech_stack WHERE html_url = :repoName)
          ORDER BY stargazers_count DESC`;
-  list = await sequelize.query(sql, {
-    replacements: { repoName: `https://github.com/${fullName}` },
-    type: sequelize.QueryTypes.SELECT,
-  });
-  for (const item of list) {
+
+  const [similarByAi, similarByCategory] = await Promise.all([
+    sequelize.query(querySimilarByAiSql, {
+      replacements: { fullName },
+      type: sequelize.QueryTypes.SELECT,
+    }),
+    sequelize.query(querySimilarByCategorySql, {
+      replacements: { repoName: `https://github.com/${fullName}` },
+      type: sequelize.QueryTypes.SELECT,
+    }),
+  ]);
+
+  const alternatives = similarByAi.map(item => ({
+    pId: item.alternative_id,
+    repoName: item.alternative_name,
+    logo: item.owner_avatar_url,
+    starCount: item.starCount,
+    forksCount: item.forksCount,
+    url: item.alternative_url,
+    description: item.description,
+    ai: item.source === 'ai' ? 1 : 0,
+  }));
+  for (const item of similarByCategory) {
     // exclude it self
     if (item.full_name === fullName) continue;
 
     const alternative = alternatives.find(e => e.repoName === item.full_name);
     if (alternative) {
-      alternative.ai = 0; // mark as non-AI
       continue; // exclude exist
     }
 
